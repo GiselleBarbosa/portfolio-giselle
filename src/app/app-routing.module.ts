dd818e3b7ad3ad26c6eafd39199bdf8fa2697133
GiselleBarbosa <g88barbosa@gmail.com>
2022-12-29 08:53:46 -0300

    created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/home/about/about.component';
+import { ContactComponent } from './pages/home/contact/contact.component';
+import { CurriculumComponent } from './pages/home/curriculum/curriculum.component';
+import { PortfolioComponent } from './pages/home/portfolio/portfolio.component';
 
 const routes: Routes = [
-
+  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'customers',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'curriculum', component: CurriculumComponent },
+  { path: 'portfolio', component: PortfolioComponent },
 ];
 
 @NgModule({
